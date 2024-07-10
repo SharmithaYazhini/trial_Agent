32f87393fd9b04f48d5a2112d9fb5fd37550de1e
Sharmitha Yazhini N M <104150250+SharmithaYazhini@users.noreply.github.com>
2024-07-10 18:39:58 -0400

    Update miller-lab-agent.py

--- miller-lab-agent.py
+++ miller-lab-agent.py
@@ -2,16 +2,10 @@ from langchain_community.callbacks import StreamlitCallbackHandler
 import streamlit as st
 
 from langchain_openai import ChatOpenAI
-from langchain.agents import  initialize_agent
-import streamlit as st
-# from langchain.document_loaders import PyMuPDFLoader
+from langchain.agents import initialize_agent
 
-import("pysqlite3")
-import sys sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
-import sqlite3
 # Build a sample vectorDB
-from langchain_community.vectorstores import Chroma
+from langchain.vectorstores import Annoy
 from langchain_openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.agents.agent_toolkits import create_retriever_tool
@@ -19,17 +13,8 @@ from langchain.agents.agent_toolkits import create_retriever_tool
 from langchain.prompts import SystemMessagePromptTemplate
 
 from langchain.memory import ConversationBufferMemory
-from langchain_community.vectorstores import FAISS
 import fitz
 from langchain_community.chat_message_histories import StreamlitChatMessageHistory
-
-
-import sqlite3
-import sys
-from sqlite3 import Error
-#sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
-
 
 st.title('👪  Caregiving Handbook')
 st.markdown('🙏🏼 Welcome to the Handbook Healthcare Assistant! We have provided the assistant with helpful information to support you in navigating the world of caregiving for children with cancer. It has access to the Children\'s Oncology Group Family Handbook, a trusted resource for pediatric oncology information. Please give it a moment to set itself up.')
@@ -67,23 +52,23 @@ def process_entire_document_for_splits(doc):
                         # Process text block with heading levels
                         current_heading_level = None
                         if font == "Archer-MediumItalic" and size == 38.0:
-                            current_heading_level = 1
+                            current_heading level = 1
                         elif font == "Archer-SemiboldItalic" and size == 12.0:
-                            current_heading_level = 2
+                            current_heading level = 2
                         elif font == "Archer-Bold" and size == 9.5:
-                            current_heading_level = 3
+                            current_heading level = 3
                         elif font == "Frutiger-Italic" and size == 9.5:
-                            current_heading_level = 4
+                            current_heading level = 4
                         
-                        if current_heading_level:
-                            page_chunks.append(f"Heading {current_heading_level}: {text}")
+                        if current heading level:
+                            page_chunks.append(f"Heading {current heading level}: {text}")
                         else:
                             page_chunks.append(f"Normal Text: {text}")
 
-        if labeled_page_number:
-                full_page_header = " ".join(page_header)
-                page_content = " ".join(page_chunks)
-                metadata = {"labeled_page_number": labeled_page_number, "page_header": full_page_header}
+        if labeled page number:
+                full page header = " ".join(page_header)
+                page content = " ".join(page_chunks)
+                metadata = {"labeled page number": labeled page number, "page header": full page header}
                 document = Document(page_content, metadata)
                 all_documents.append(document)
 
@@ -91,8 +76,6 @@ def process_entire_document_for_splits(doc):
 
 # Load the PDF document
 data = fitz.open("English_COG_Family_Handbook.pdf")
-# data = loader.load()
-
 
 # Process the document and create splits
 document_splits = process_entire_document_for_splits(data)
@@ -101,9 +84,8 @@ openai_key = st.secrets["andrew_openai_api_key"]
 
 # VectorDB setup
 embedding = OpenAIEmbeddings(openai_api_key=openai_key)
-vectordb = Chroma.from_documents(documents=document_splits, embedding=embedding)
+vectordb = Annoy.from_documents(documents=document_splits, embedding=embedding)
 retriever = vectordb.as_retriever()
-
 
 # Tool
 HandbookTool = create_retriever_tool(
@@ -112,26 +94,20 @@ HandbookTool = create_retriever_tool(
     "A tool to get relevant information from the children's oncology group family handbook.",
 )
 
-
 # Initialize chat history
 if 'messages' not in st.session_state:
     # Start with first message from assistant
     st.session_state['messages'] = [{"role": "assistant", 
                                   "content": "Hi, How can I help!"}]
 
-
-llm = ChatOpenAI( model_name="gpt-4-1106-preview", temperature=0, streaming=True,openai_api_key=openai_key)
-
+llm = ChatOpenAI(model_name="gpt-4", temperature=0, streaming=True, openai_api_key=openai_key)
 
 tools = [HandbookTool]
-# agent = create_conversational_retrieval_agent(llm, tools, verbose=True)
 
 if 'chat_history' not in st.session_state:
     st.session_state.chat_history = []
 msgs = StreamlitChatMessageHistory()
 memory = ConversationBufferMemory(memory_key='chat_history', chat_memory=msgs, return_messages=True)
-# Create memory 'chat_history' 
-
 
 system_prompt_template = SystemMessagePromptTemplate.from_template("""
 You are a Handbook Healthcare Assistant with access to the Children's Oncology Group Family Handbook.
@@ -144,10 +120,8 @@ agent = initialize_agent(
     memory=memory
 )
 
-
 st_callback = StreamlitCallbackHandler(st.container())
 company_logo="https://www.iu.edu/images/brand/brand-expression/iu-trident-promo.jpg"
-
 
 # Display chat messages from history on app rerun
 # Custom avatar for the assistant, default avatar for user
@@ -159,9 +133,8 @@ for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-
 format = """
-Make sure the answer is primarily based only on the document pages retireved. The answer should be dense with citations to the orginal material. Keep the answer about one paragraph long.
+Make sure the answer is primarily based only on the document pages retrieved. The answer should be dense with citations to the original material. Keep the answer about one paragraph long.
 - Include inline citations in square brackets within the paragraph.
 - List all references at the end under 'References' with the format: 'Most relevant heading, **Page Header, Page Number**'.
 
@@ -180,13 +153,6 @@ Agent Response: {
 }
 """
 
-
-
-
-
-
-
-
 # Chat logic
 if query := st.chat_input("Ask me anything"):
     # Add user message to chat history
@@ -202,9 +168,5 @@ if query := st.chat_input("Ask me anything"):
         response = agent.invoke({"input": query+format, "chat_history": st.session_state.chat_history}, config={"callbacks":[st_callback]})["output"]
         st.session_state.chat_history.append({"role": "assistant", "content": response})
         st.markdown(response)
-        interaction = (user_id, query, response)
-        #insert_interaction(conn, interaction)
-        #with open("interaction_logs.txt", "a") as file:
-        #    file.write(f"User ID: {user_id}, Message Prompt: {query}, Output: {response}\n")
     # Add assistant message to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
