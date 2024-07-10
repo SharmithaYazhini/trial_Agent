ce58082d8aa232e034396b253ab8d3ecccc10d0a
Sharmitha Yazhini N M <104150250+SharmithaYazhini@users.noreply.github.com>
2024-07-10 17:57:43 -0400

    Update miller-lab-agent.py

--- miller-lab-agent.py
+++ miller-lab-agent.py
@@ -1,11 +1,11 @@
+
 from langchain_community.callbacks import StreamlitCallbackHandler
 import streamlit as st
 
 from langchain_openai import ChatOpenAI
-from langchain.agents import  initialize_agent
+from langchain.agents import initialize_agent
 import streamlit as st
 # from langchain.document_loaders import PyMuPDFLoader
-
 
 # Build a sample vectorDB
 from langchain_community.vectorstores import Chroma
@@ -19,14 +19,6 @@ from langchain.memory import ConversationBufferMemory
 from langchain_community.vectorstores import FAISS
 import fitz
 from langchain_community.chat_message_histories import StreamlitChatMessageHistory
-
-
-import sqlite3
-import sys
-from sqlite3 import Error
-#sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
-
 
 st.title('👪  Caregiving Handbook')
 st.markdown('🙏🏼 Welcome to the Handbook Healthcare Assistant! We have provided the assistant with helpful information to support you in navigating the world of caregiving for children with cancer. It has access to the Children\'s Oncology Group Family Handbook, a trusted resource for pediatric oncology information. Please give it a moment to set itself up.')
@@ -88,8 +80,6 @@ def process_entire_document_for_splits(doc):
 
 # Load the PDF document
 data = fitz.open("English_COG_Family_Handbook.pdf")
-# data = loader.load()
-
 
 # Process the document and create splits
 document_splits = process_entire_document_for_splits(data)
@@ -101,7 +91,6 @@ embedding = OpenAIEmbeddings(openai_api_key=openai_key)
 vectordb = Chroma.from_documents(documents=document_splits, embedding=embedding)
 retriever = vectordb.as_retriever()
 
-
 # Tool
 HandbookTool = create_retriever_tool(
     retriever,
@@ -109,26 +98,20 @@ HandbookTool = create_retriever_tool(
     "A tool to get relevant information from the children's oncology group family handbook.",
 )
 
-
 # Initialize chat history
 if 'messages' not in st.session_state:
     # Start with first message from assistant
     st.session_state['messages'] = [{"role": "assistant", 
                                   "content": "Hi, How can I help!"}]
 
-
-llm = ChatOpenAI( model_name="gpt-4-1106-preview", temperature=0, streaming=True,openai_api_key=openai_key)
-
+llm = ChatOpenAI(model_name="gpt-4o", temperature=0, streaming=True,openai_api_key=openai_key)
 
 tools = [HandbookTool]
-# agent = create_conversational_retrieval_agent(llm, tools, verbose=True)
 
 if 'chat_history' not in st.session_state:
     st.session_state.chat_history = []
 msgs = StreamlitChatMessageHistory()
 memory = ConversationBufferMemory(memory_key='chat_history', chat_memory=msgs, return_messages=True)
-# Create memory 'chat_history' 
-
 
 system_prompt_template = SystemMessagePromptTemplate.from_template("""
 You are a Handbook Healthcare Assistant with access to the Children's Oncology Group Family Handbook.
@@ -141,10 +124,8 @@ agent = initialize_agent(
     memory=memory
 )
 
-
 st_callback = StreamlitCallbackHandler(st.container())
 company_logo="https://www.iu.edu/images/brand/brand-expression/iu-trident-promo.jpg"
-
 
 # Display chat messages from history on app rerun
 # Custom avatar for the assistant, default avatar for user
@@ -155,7 +136,6 @@ for message in st.session_state.messages:
     else:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
-
 
 format = """
 Make sure the answer is primarily based only on the document pages retireved. The answer should be dense with citations to the orginal material. Keep the answer about one paragraph long.
@@ -177,46 +157,6 @@ Agent Response: {
 }
 """
 
-
-# Database logic
-def create_connection(db_file):
-    """ create a database connection to a SQLite database """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        return conn
-    except Error as e:
-        print(e)
-    return conn
-
-def create_table(conn):
-    try:
-        c = conn.cursor()
-        c.execute('''CREATE TABLE IF NOT EXISTS user_interactions
-                     (user_id TEXT, input_message TEXT, output_message TEXT)''')
-    except Error as e:
-        print(e)
-
-def insert_interaction(conn, interaction):
-    sql = ''' INSERT INTO user_interactions(user_id,input_message,output_message)
-              VALUES(?,?,?) '''
-    cur = conn.cursor()
-    cur.execute(sql, interaction)
-    conn.commit()
-
-database = "C:/Users/shanata/Desktop/miller/miller-lab-agent/data.db"
-
-conn = create_connection(database)
-if conn is not None:
-    create_table(conn)
-else:
-    print("Error! cannot create the database connection.")
-
-
-
-
-
-
 # Chat logic
 if query := st.chat_input("Ask me anything"):
     # Add user message to chat history
@@ -232,10 +172,6 @@ if query := st.chat_input("Ask me anything"):
         response = agent.invoke({"input": query+format, "chat_history": st.session_state.chat_history}, config={"callbacks":[st_callback]})["output"]
         st.session_state.chat_history.append({"role": "assistant", "content": response})
         st.markdown(response)
-        interaction = (user_id, query, response)
-        insert_interaction(conn, interaction)
-        #with open("interaction_logs.txt", "a") as file:
-        #    file.write(f"User ID: {user_id}, Message Prompt: {query}, Output: {response}\n")
     # Add assistant message to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
 
